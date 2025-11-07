a681282144d2a91a4f12b287d5dd3abf8cd5cb76
Vibin <vibinvigneshs@gmail.com>
2025-11-07 13:55:46 +0530

    ticket

---
+++ ticket/ticket.js
@@ -0,0 +1,6 @@
+function open(){
+                var a = document.createElement("a")
+                a.href = "https://wa.me/?text=https://example.com/ticket/12345"
+                div.append(a)
+                
+            }
