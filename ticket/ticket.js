817eb33a9156f3caa36ae02419a49c0cd4892732
Vibin <vibinvigneshs@gmail.com>
2025-11-07 14:00:39 +0530

    ticket deleted

--- ticket/ticket.js
+++
@@ -1,6 +0,0 @@
-function open(){
-                var a = document.createElement("a")
-                a.href = "https://wa.me/?text=https://example.com/ticket/12345"
-                div.append(a)
-                
-            }
